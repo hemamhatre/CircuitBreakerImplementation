9004092b11ad488a9a0e4c06cc380518fca4b490
Hema <hemamhatre80@yahoo.co.in>
2018-02-21 11:37:04 +0530

    Added files
    Added files

---
+++ CircuitBreakerConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircuitBreakerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test objTest = new Test();
+           Console.WriteLine( objTest.TestCirciutBreaker());
+           Console.WriteLine(objTest.TestCirciutBreaker());
+           Console.WriteLine(objTest.TestCirciutBreaker());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CircuitBreakerConsole/Test.cs
@@ -0,0 +1,36 @@
+﻿
+using BaseCircuitBreaker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircuitBreakerConsole
+{
+  public  class Test
+    {
+        private static readonly CircuitBreaker mycircuitBreaker = new CircuitBreaker(3, TimeSpan.FromMinutes(15));
+
+        public string TestCirciutBreaker()
+        {
+            if (mycircuitBreaker.AttemptCall(() => { DoSomething(); }).IsClosed)
+            {
+                return "Called code";
+            }
+            else
+                return "Too many failures ,Resource not available";
+        }
+
+        public void DoSomething()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Doing something");
+                throw new NotImplementedException();
+            }
+        }
+
+
+    }
+}
